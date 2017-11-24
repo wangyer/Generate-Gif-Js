c491c0cad4903908b9efff587004ac6399929100
wangye9323 <wangye9323@autohome.com.cn>
2017-11-24 16:21:35 +0800

    Two methods of generating GIF images through JS

---
+++ gif/js/json.js
@@ -0,0 +1,49 @@
+var data = {
+	message: "",
+	returnCode: 0,
+	imgList: [
+		[
+			"images/1.jpg",
+			"images/2.jpg",
+			"images/3.jpg",
+			"images/4.jpg",
+			"images/5.jpg",
+			"images/6.jpg"
+		],[
+			"images/1.jpg",
+			"images/2.jpg",
+			"images/3.jpg",
+			"images/4.jpg",
+			"images/5.jpg",
+			"images/6.jpg"
+		],[
+			"images/1.jpg",
+			"images/2.jpg",
+			"images/3.jpg",
+			"images/4.jpg",
+			"images/5.jpg",
+			"images/6.jpg"
+		],[
+			"images/1.jpg",
+			"images/2.jpg",
+			"images/3.jpg",
+			"images/4.jpg",
+			"images/5.jpg",
+			"images/6.jpg"
+		],[
+			"images/1.jpg",
+			"images/2.jpg",
+			"images/3.jpg",
+			"images/4.jpg",
+			"images/5.jpg",
+			"images/6.jpg"
+		],[
+			"images/1.jpg",
+			"images/2.jpg",
+			"images/3.jpg",
+			"images/4.jpg",
+			"images/5.jpg",
+			"images/6.jpg"
+		]
+	]
+}
